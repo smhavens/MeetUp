30b1b56301571e94f4aa190553a508f31f19126c
smhavens <smhavens@umich.edu>
2024-11-26 21:23:59 -0500

    New Updates, all attributes for new account added and further details for creating event.

--- createAccount_ui.py
+++ createAccount_ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'createAccountyAhHTY.ui'
+## Form generated from reading UI file 'createAccount.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.0
 ##
